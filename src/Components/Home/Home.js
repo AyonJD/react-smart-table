3001ffdf233147539cbfa2d869f197caa51c8207
Ayon <ayonjodder177@gmail.com>
2022-06-10 11:38:01 +0600

    fetch for userCount and PageCount

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  const [allUsersCount, setAllUsersCount] = useState(0);
-  const [allUsers, setAllUsers] = useState([]);
+    const [allUsersCount, setAllUsersCount] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
+    const [pageSize, setPageSize] = useState("10");
+    const [allUsers, setAllUsers] = useState([]);
+
+    useEffect(() => {
+        if (pageSize === "10") {
+            fetch(`https://react-smart-data-table.herokuapp.com/users/count`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setPageCount(Math.ceil(data.count / 10));
+                    setAllUsersCount(data.count);
+                });
+        }
+    }, [pageCount, pageSize]);
 
 
-  useEffect(() => {
-    fetch(`https://react-smart-data-table.herokuapp.com/users`)
-      .then((res) => res.json())
-      .then((data) => {
-        setAllUsers(data);
-      });
-  }, [allUsersCount]);
+    useEffect(() => {
+        fetch(`https://react-smart-data-table.herokuapp.com/users`)
+            .then((res) => res.json())
+            .then((data) => {
+                setAllUsers(data);
+            });
+    }, [allUsersCount]);
 
-  return (
-      <div className="my-5">
-          {allUsers.length}
-    </div>
-  );
+    return (
+        <div className="my-5">
+            {allUsers.length}
+        </div>
+    );
 };
 
 export default Home;
